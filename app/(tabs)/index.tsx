7d9a8722a3f98bac0dfa26ca534cac7b7ae358f7
Suhee Kim <tngml118@gmail.com>
2024-11-27 16:23:43 -0700

    Create Card, profile, and title components

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,15 @@
-import { View, Text } from "react-native";
 import { Colors } from "@/constants/Colors";
+import CategoryCard from "@/src/components/card/CategoryCard";
+import QuizCard from "@/src/components/card/QuizCard";
+import ProfileImage from "@/src/components/profile/ProfileImage";
+import ProfileUsername from "@/src/components/profile/ProfileUsername";
+import { View } from "react-native";
 
 export default function HomeScreen() {
+  const handlePress = () => {
+    console.log("Pressed");
+  };
+
   return (
     <View
       style={{
@@ -11,7 +19,24 @@ export default function HomeScreen() {
         alignItems: "center",
       }}
     >
-      <Text style={{ fontFamily: "Rubik_600SemiBold", color: "white" }}>Home Page</Text>
+      <ProfileImage imageUrl="https://www.aiscribbles.com/img/variant/large-preview/32046/?v=7ce9ca" />
+      <ProfileUsername
+        imageUrl="https://www.aiscribbles.com/img/variant/large-preview/32046/?v=7ce9ca"
+        username="John Doe"
+      />
+      <QuizCard
+        title={"Statistic Quiz"}
+        imageUrl={"https://www.aiscribbles.com/img/variant/large-preview/32046/?v=7ce9ca"}
+        onPress={handlePress}
+        category={"Math"}
+        numberOfQuestions={"12 Questions"}
+      />
+      <CategoryCard
+        categoryName="Japanese Food"
+        onPress={handlePress}
+        numberOfQuestions="12 Questions"
+        imageUrl="https://www.aiscribbles.com/img/variant/large-preview/32046/?v=7ce9ca"
+      />
     </View>
   );
 }
---
+++ src/components/card/QuizCard.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import CardTitle from "../title/CardTitle";
+import CardContainer from "./CardContainer";
+import CardImage from "./CardImage";
+
+export default function QuizCard({
+  title,
+  imageUrl,
+  onPress,
+  category,
+  numberOfQuestions,
+}: {
+  title: string;
+  imageUrl: string;
+  onPress: () => void;
+  category: string;
+  numberOfQuestions: string;
+}) {
+  return (
+    <CardContainer onPress={onPress} style={styles.cardContainer}>
+      <CardImage style={styles.imageContainer} imageUrl={imageUrl} />
+      <View style={styles.infoContainer}>
+        <CardTitle cardTitle={title} />
+        <Text style={styles.details}>
+          {category} | {numberOfQuestions}
+        </Text>
+      </View>
+    </CardContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  cardContainer: {
+    display: "flex",
+    flexDirection: "row",
+  },
+  imageContainer: {
+    marginRight: 10,
+  },
+  infoContainer: {
+    justifyContent: "center",
+  },
+  details: {
+    fontSize: 14,
+    color: "gray",
+  },
+});
