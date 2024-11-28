229fcc2a90e8fc2ebbc995175db82f72a0a09a1f
Suhee Kim <144300312+Suhee-K@users.noreply.github.com>
2024-11-28 16:08:32 -0700

    Add Quiz Section in index.tsx

--- app/screens/index.tsx
+++ app/screens/index.tsx
@@ -1,6 +1,7 @@
 import { Colors } from "@/constants/Colors";
 import QuizCard from "@/src/components/card/QuizCard";
-import { View } from "react-native";
+import SectionTitle from "@/src/components/text/SectionTitle";
+import { FlatList, ScrollView, StyleSheet, Text, View } from "react-native";
 
 export default function HomeScreen() {
   const handlePress = () => {
@@ -12,17 +13,64 @@ export default function HomeScreen() {
       style={{
         flex: 1,
         backgroundColor: Colors.light.accent,
-        justifyContent: "center",
+        justifyContent: "flex-end",
         alignItems: "center",
       }}
     >
-      <QuizCard
-        title={"Statistic Quiz"}
-        imageUrl={"https://www.aiscribbles.com/img/variant/large-preview/32046/?v=7ce9ca"}
-        onPress={handlePress}
-        category={"Math"}
-        numberOfQuestions={12}
-      />
+      <View style={styles.quizContainer}>
+        <ScrollView style={styles.scrollView}>
+          <SectionTitle sectionTitle="Most Popular" />
+          <QuizCard
+            title={"Statistic Quiz"}
+            imageUrl={"https://www.aiscribbles.com/img/variant/large-preview/32046/?v=7ce9ca"}
+            onPress={handlePress}
+            category={"Math"}
+            numberOfQuestions={12}
+          />
+          <QuizCard
+            title={"Statistic Quiz"}
+            imageUrl={"https://www.aiscribbles.com/img/variant/large-preview/32046/?v=7ce9ca"}
+            onPress={handlePress}
+            category={"Math"}
+            numberOfQuestions={12}
+          />
+          <QuizCard
+            title={"Statistic Quiz"}
+            imageUrl={"https://www.aiscribbles.com/img/variant/large-preview/32046/?v=7ce9ca"}
+            onPress={handlePress}
+            category={"Math"}
+            numberOfQuestions={12}
+          />
+          <QuizCard
+            title={"Statistic Quiz"}
+            imageUrl={"https://www.aiscribbles.com/img/variant/large-preview/32046/?v=7ce9ca"}
+            onPress={handlePress}
+            category={"Math"}
+            numberOfQuestions={12}
+          />
+          <QuizCard
+            title={"Statistic Quiz"}
+            imageUrl={"https://www.aiscribbles.com/img/variant/large-preview/32046/?v=7ce9ca"}
+            onPress={handlePress}
+            category={"Math"}
+            numberOfQuestions={12}
+          />
+        </ScrollView>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  scrollView: {
+    marginHorizontal: 10,
+  },
+
+  quizContainer: {
+    width: "100%",
+    height: "65%",
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    backgroundColor: "white",
+  },
+});
--- src/components/text/SectionTitle.tsx
+++ src/components/text/SectionTitle.tsx
@@ -1,6 +1,16 @@
 import React from "react";
-import { Text } from "react-native";
+import { StyleSheet, Text } from "react-native";
 
 export default function SectionTitle({ sectionTitle }: { sectionTitle: string }) {
-  return <Text>{sectionTitle}</Text>;
+  return <Text style={styles.text}>{sectionTitle}</Text>;
 }
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginHorizontal: 10,
+    marginTop: 20,
+    marginBottom: 10,
+  },
+});
