063c83faae2b94221311eb0078c13a9e8ec9eacf
-srntz <tuzsocial909@gmail.com>
2024-11-28 14:55:05 -0700

    Add authentication context and user data interface

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,6 +9,8 @@ import { useFonts } from "@expo-google-fonts/rubik";
 import { Rubik_600SemiBold } from "@expo-google-fonts/rubik";
 import QuizzySplashScreen from "@/src/components/utils/QuizzySplashScreen";
 import React from "react";
+import { AuthenticationContext, IAuthenticationContext } from "@/src/context/AuthenticationContext";
+import { IUserData } from "@/src/interfaces/IUserData";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -17,6 +19,8 @@ export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({ Rubik_600SemiBold });
   const [customSplashScreen, setCustomSplashScreen] = useState(false);
+
+  const [currentUser, setCurrentUser] = useState<IUserData | undefined>(undefined);
 
   useEffect(() => {
     async function splashScreenTimeout() {
@@ -35,13 +39,15 @@ export default function RootLayout() {
     return <QuizzySplashScreen ready={true} />;
   } else {
     return (
-      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-        <Stack>
-          <Stack.Screen name={"login"} options={{ headerShown: false }} />
-          <Stack.Screen name={"screens"} options={{ headerShown: false }} />
-        </Stack>
-        <StatusBar style="auto" />
-      </ThemeProvider>
+      <AuthenticationContext.Provider value={{ data: currentUser, setCurrentUser: setCurrentUser }}>
+        <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+          <Stack>
+            <Stack.Screen name={"login"} options={{ headerShown: false }} />
+            <Stack.Screen name={"screens"} options={{ headerShown: false }} />
+          </Stack>
+          <StatusBar style="auto" />
+        </ThemeProvider>
+      </AuthenticationContext.Provider>
     );
   }
 }
